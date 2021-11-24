ff065bc4e9856488051c2537db423621ce6a1cb3
Ahmed Abdelaal <a.abdelaal@protonmail.com>
2021-11-24 20:12:40 +0300

    Fixed warning message: "Auto-created primary key used when not defining a primary key type"

--- auctions/apps.py
+++ auctions/apps.py
@@ -2,4 +2,7 @@ from django.apps import AppConfig
 
 
 class AuctionsConfig(AppConfig):
+    # https://stackoverflow.com/questions/67783120/warning-auto-created-primary-key-used-when-not-defining-a-primary-key-type-by
+    default_auto_field = 'django.db.models.AutoField'
+    
     name = 'auctions'
